99e0a199f5f4c2294ebb5b0a4b367a8660c5d661
kritbovorn <ktaweeyossak@yahoo.com>
2021-03-10 15:54:02 +0700

    finish create generate Class, Component Of Product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './Product/product-list/product-list.component';
+import { ProductItemComponent } from './Product/product-item/product-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule,
